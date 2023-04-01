e7bdc11f4f20f553bdab87b5ff2518233e927643
Shui <zhongshui.wang.chn@gmail.com>
2023-04-01 22:30:25 +0200

    use swagger 3

---
+++ arts/src/main/java/com/system/arts/config/OpenAPIConfig.java
@@ -0,0 +1,41 @@
+package com.system.arts.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenAPIConfig {
+
+  @Value("${artsSystem.openapi.dev-url}")
+  private String devUrl;
+
+  @Value("${artsSystem.openapi.prod-url}")
+  private String prodUrl;
+
+  @Bean
+  public OpenAPI myOpenAPI() {
+    Server devServer = new Server();
+    devServer.setUrl(devUrl);
+    devServer.setDescription("Server URL in Development environment");
+
+    Server prodServer = new Server();
+    prodServer.setUrl(prodUrl);
+    prodServer.setDescription("Server URL in Production environment");
+
+    Info info = new Info()
+        .title("美术艺考教学资源系统API")
+        .version("1.0")
+        .description("This API exposes endpoints to manage arts system.");
+
+    return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
+  }
+}
