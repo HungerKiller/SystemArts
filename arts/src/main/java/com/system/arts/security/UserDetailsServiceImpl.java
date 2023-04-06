c5e55e643ec12069fa626ee4c28729e0972ac67a
Shui <zhongshui.wang.chn@gmail.com>
2023-04-06 21:46:42 +0200

    encode password

--- arts/src/main/java/com/system/arts/security/UserDetailsServiceImpl.java
+++ arts/src/main/java/com/system/arts/security/UserDetailsServiceImpl.java
@@ -1,9 +1,12 @@
 package com.system.arts.security;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.system.arts.entity.User;
@@ -14,11 +17,13 @@ import com.system.arts.service.UserService;
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserService userService;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userService.getUserByName(username);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return new UserDetailsImpl(user);
     }
 }
-
-
